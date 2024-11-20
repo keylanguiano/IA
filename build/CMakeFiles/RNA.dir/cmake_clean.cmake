ec0bcbe342e57cd36feb16f70fe322d69105b7fe
keylanguiano <keyla_anguiano@hotmail.com>
2024-11-19 19:41:44 -0600

    Boosting the neural network’s accuracy

---
+++ build/CMakeFiles/RNA.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RNA.dir/RNA.cpp.o"
+  "CMakeFiles/RNA.dir/RNA.cpp.o.d"
+  "RNA"
+  "RNA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RNA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
